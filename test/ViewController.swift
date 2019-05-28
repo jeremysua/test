242780bad78e56e28130fba96fad6af04de11670
Jeremy Su'a <jeremysua@hotmail.com>
2019-05-28 18:42:32 +0200

    first commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var hello = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        hello = true
     }
 
 
